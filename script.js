e92d27f62b4dcae855d3669da00325165b4225e1
Mikkel Lehrmann <mikkellehrmann@Mikkels-MacBook-Pro-3.local>
2018-04-21 15:05:20 +0200

    ya

---
+++ script.js
@@ -0,0 +1,30 @@
+let jsonUrl = "http://mixel.dk/kea/food8/wordpress/wp-json/wp/v2/steder";
+        let steder = [];
+
+        let display = document.querySelector("section");
+        let template = document.querySelector("template");
+
+        document.addEventListener("DOMContentLoaded", hentJson);
+
+        async function hentJson() {
+            let dataJson = await fetch(jsonUrl);
+            steder = await dataJson.json();
+            console.log(steder);
+
+            visSteder();
+
+        }
+
+        function visSteder() {
+            steder.forEach(sted => {
+                let klon = template.cloneNode(true).content;
+
+                klon.querySelector("[data-header]").textContent = sted.title.rendered;
+
+                klon.querySelector("[data-image]").src = sted.acf.logo.url;
+
+                klon.querySelector("[data-image]").alt = sted.title.rendered;
+
+                display.appendChild(klon);
+            });
+        }
