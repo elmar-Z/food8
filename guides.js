6cd2642082bb1a1aa2eb36a31188be2ba71837ae
Mikkel Lehrmann <mikkellehrmann@Mikkels-MacBook-Pro-3.local>
2018-04-23 19:11:40 +0200

    de

---
+++ guides.js
@@ -0,0 +1,34 @@
+        let jsonUrl = "http://mixel.dk/kea/food8/wordpress/wp-json/wp/v2/guide";
+        let steder = [];
+
+        let display = document.querySelector("section");
+        let template = document.querySelector("template");
+
+        document.addEventListener("DOMContentLoaded", hentJson);
+
+        async function hentJson() {
+            let dataJson = await fetch(jsonUrl);
+            steder = await dataJson.json();
+            console.log(steder);
+
+            visSteder();
+
+        }
+
+        function visSteder() {
+            steder.forEach(sted => {
+                let klon = template.cloneNode(true).content;
+
+                klon.querySelector("[data-header]").textContent = sted.title.rendered;
+
+                klon.querySelector("[data-image]").src = sted.acf.billede.url;
+
+                klon.querySelector("[data-image]").alt = sted.title.rendered;
+
+                klon.querySelector("article").addEventListener("click", () => {
+                     location.href = "single-guide.html?id=" + sted.id;
+                 });
+
+                display.appendChild(klon);
+            });
+        }
---
+++ single-guide.js
@@ -0,0 +1,35 @@
+let urlParams = new URLSearchParams(window.location.search);
+let urlID = urlParams.get('id');
+
+let jsonUrl = "http://mixel.dk/kea/food8/wordpress/wp-json/wp/v2/guide";
+let steder = [];
+
+
+document.addEventListener("DOMContentLoaded", hentJson);
+
+        async function hentJson() {
+            let dataJson = await fetch(jsonUrl);
+            steder = await dataJson.json();
+            console.log(steder);
+
+            visSteder();
+
+        }
+
+        function visSteder() {
+            steder.forEach(sted => {
+
+                if (sted.id == urlID) {
+                    console.log(sted.id, urlID);
+
+                document.querySelector("[data-header]").textContent = sted.title.rendered;
+
+                document.querySelector("[data-image]").src = sted.acf.billede.url;
+
+                document.querySelector("[data-image]").alt = sted.title.rendered;
+
+                document.querySelector("[data-description]").innerHTML = sted.content.rendered;
+
+                }
+            });
+        }
